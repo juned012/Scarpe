91239f8f80f8417bf3b45dff0575abfd48e78e97
Faiyan khan <144541895+faiyankhan@users.noreply.github.com>
2024-12-22 14:50:55 +0000

    Functionality added in sideshow

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const images = ["img/first.webp", "img/second.webp", "img/third.jpg", "img/fourth.webp", "img/fifth.jpg", "img/sixth.jpg"];
+
+let index = 0;
+const slide = document.getElementById('slide');
+
+function next() {
+    if (index < images.length - 1) {
+        index++;
+    }
+    else {
+        index = 0;
+    }
+    slide.src = images[index];
+}
+
+function previous() {
+    if (index > 0) {
+        index--;
+    }
+    else {
+        index = images.length - 1;
+    }
+    slide.src = images[index];
+}
+
+setInterval(next, 3000);
